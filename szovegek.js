5af54d72bc49acd7d12128300b716e23c6941927
Csongor Beres Duha <beresd.csongor@yahoo.com>
2021-02-13 04:13:32 +0200

    setting up repo

---
+++ szovegek.js
@@ -0,0 +1,96 @@
+const page_aboutus = {
+	"en": {
+		"title": "About us",
+		"part_1": "We love sports. We admire how sport teaches us many great things like motivation, courage to follow our dreams and respect. We appreciate how sport gives us friendships for life. We are impressed by the journey of a curious kid with bright eyes who becomes a champion, and by the journey of a small team for kids which is built into a strong sport club through results, wins and losses and hard work.",
+		"part_2": "Our goal is to help people and organizations in sports area, who are willing to do and willing to progress-they are those who we call professionals, either they are small or big. We offer personalized solutions for them.",
+		"list_header": "Our main services include:",
+		"list_el_1": "consultancy for clubs, individual sportsman, city halls and public institutions, sport academies, sport organizations",
+		"list_el_2": "sport management & sport marketing services",
+		"list_el_3": "sport strategy development",
+		"list_el_4": "organizing sport events",
+		"list_el_5": "professional training and courses for sport specialists, coaches, managers, club presidents",
+		"list_el_6": "project planning for promoting a healthy lifestyle",
+		"part_3": "I’m a husband and a father; moreover, a sports manager and organizer. After I finished my career as a basketball player, I have become a refree, a basketball coach and a sports agent. Whatever I’ve learned, whatsoever businesses I’ve done through years, I always returned to basketball. I love brainstorming: I like thinking about how to solve a problem in sports creatively. My main personal interests – sport, management and the business world – intersect in sports management.",
+		"part_4": "As a sports manager, I help clubs and sportsmen to achieve their goals and to set new ones, to build a cohesive community with their fans and supporters, and to become a strong brand. We can plan such projects together with municipalities, associations and sport federations that will boost mass sport with the help of professional sport, encourage healthy lifestyle and draw more and more children to trainings. I see the future of the sport in these children, that’s why talent promotion and organizing such events that offer opportunities are in my heart."
+	},
+	"ro": {
+		"title": "Despre noi",
+		"part_1": "Iubim sportul. Suntem impresionați de felul în care sportul ne învață atâtea lucruri de neprețuit în viață: cum să fim motivați, curajoși să ne urmăm visele și să ne respectăm atât prietenii, cât și adversarii. Apreciem prieteniile durabile pe care ni le oferă. Admirăm drumul care duce de la primul antrenament al unui copil curios,cu privirea strălucitoare, până la momentul în care el devine un campion și drumul unei echipe mici de copii care ajunge un club mare și puternic prin efortul depus, rezultate, victorii si înfrângeri.",
+		"part_2": "Misiunea nostră este de a-i ajuta pe specialiști și organizațiile din sfera sportului care sunt dornici de a lucra și de a progresa- noi îi numim profesioniști, fie ei mici sau mari. Compania noastră le oferă soluții personalizate.",
+		"list_header": "Serviciile noastre:",
+		"list_el_1": "consultanță pentru cluburi, sportivi, primării și instituții publice, academii, organizații și asociații sportive",
+		"list_el_2": "servicii de marketing și de management sportiv",
+		"list_el_3": "elaborare strategii pentru dezvoltarea sportului",
+		"list_el_4": "organizare de evenimente sportive",
+		"list_el_5": "formare profesională pentru specialiștii din domeniul sportului (antrenori, manageri, președinți de cluburi)",
+		"list_el_6": "planificare proiecte pentru promovarea unui stil de viață sănătos",
+		"part_3": "Sunt soț, tată și manager sportiv. Fost jucător de baschet, arbitru și antrenor, impresar- am revenit întotdeauna la baschet, indiferent de domeniul  în care am lucrat de-a lungul anilor. Îmi place să caut soluții creative și originale la problemele apărute în sport, iar în managementul sportiv am găsit cele trei domenii care mă pasionează: sportul, managementul și businessul.",
+		"part_4": "Ca manager sportiv, ghidez cluburile și sportivii să-și atingă și să-și depășească obiectivele, să devină un “brand” de renume și să creeze o comunitate club-sponsori-suporteri. Colaborând cu instituțiile publice, asociații și federații de specialitate, ne dorim derularea unor proiecte prin care, cu ajutorul sportului de performanță, să conștientizăm importanța sportului de masă și atragerea a cât mai multor copii în sălile de sport. În opinia mea, viitorul sportului depinde de acești copii, de aceea îmi doresc să mă implic cât mai mult în a le oferi o șansă de afirmare."
+	},
+	"hu": {
+		"title": "Rólunk",
+		"part_1": "Szeretjük a sportot. Csodáljuk, hogy mennyi mindenre tanít minket, például motivációra, bátorságra az álmaink követéséhez és tiszteletre. Hálásak vagyunk azokért az életre szóló barátságokért, melyek a sportban köttetnek. Ámulatba ejt annak az útnak a szépsége, amely az első edzéstől a bajnoki címig vezeti a csillogó szemű, kíváncsi kisgyereket, és annak a kis sportcsapatnak az utazása, mely erős és nagy klubbá válik a befektetett munka, az eredmények, a sikerek és kudarcok által.",
+		"part_2": "Célunk, hogy segítséget nyújtsunk a tenni akaróknak a sport szférájában - ők azok, akiket profiknak nevezünk, legyenek kicsik vagy nagyok. Személyre szabott megoldásokat kínálunk nekik.",
+		"list_header": "Szolgáltatásaink:",
+		"list_el_1": "tanácsadás sportklubok, egyéni sportolók, önkormányzatok és közintézmények, akadémiák, sportszervezetek és egyesületek számára;",
+		"list_el_2": "sportmarketing és sportmenedzsment szolgáltatások",
+		"list_el_3": "sportstratégia kidolgozása",
+		"list_el_4": "sportesemények szervezése",
+		"list_el_5": "szakmai továbbképzések sportszakemberek számára (edzők, menedzserek, klubelnökök)",
+		"list_el_6": "az egészséges életmódot támogató projektek tervezése",
+		"part_3": "Apa és férj vagyok, sportmenedzser- és szervező. Kosárlabda játékosból bíró, edző, majd sportügynök lettem. Bármit tanultam, bármilyen szférában tevékenykedtem az évek során, mindig visszatértem a kosárlabdához. Szeretek ötletelni, sokat gondolkodom azon, hogyan lehetne egy-egy sportbeli problémát kreatívan megoldani.  A sportmenedzsmentben találkoznak fő érdeklődési köreim: a sport, a menedzsment és az üzleti világ.",
+		"part_4": "Sportmenedzserként abban segítek a kluboknak és a sportolóknak, hogy elérjék a céljaikat, és újabb célokat tűzzenek ki, szurkolóik és támogatóik által egy összetartó közösséget építsenek maguk köré, és egy erős branddé váljanak. Az önkormányzatokkal, egyesületekkel és szakszövetségekkel együttműködve olyan projekteket tervezhetünk, amelyek a profi sport segítségével a tömegsportot is fellendítik, egészséges életmódra buzdítanak, és egyre több gyereket vonzanak be a sporttermekbe. A sport jövőjét ezekben a gyerekekben látom, ezért szívügyem a tehetséggondozást és az esélyek biztosítását célzó megmozdulások szervezése."
+	}
+}
+
+document.getElementById("angol").checked = true;
+
+let pg_aboutus = document.getElementById("About_us");
+let filler = pg_aboutus.querySelectorAll('[data-key]')
+filler.forEach(element => {
+	let key = element.getAttribute('data-key');
+	element.innerHTML = page_aboutus.en[key];
+})
+
+function switch_lang_hu(){
+	if(document.getElementById("magyar").checked == false){
+		document.getElementById("magyar").checked = true;
+	}
+	document.getElementById("roman").checked = false;
+	document.getElementById("angol").checked = false;
+
+	let pg_aboutus = document.getElementById("About_us");
+	let filler = pg_aboutus.querySelectorAll('[data-key]')
+	filler.forEach(element => {
+		let key = element.getAttribute('data-key');
+		element.innerHTML = page_aboutus.hu[key];
+	})
+}
+function switch_lang_ro(){
+		if(document.getElementById("roman").checked == false){
+			document.getElementById("roman").checked = true;
+		}
+		document.getElementById("magyar").checked = false;
+		document.getElementById("angol").checked = false;
+
+	let pg_aboutus = document.getElementById("About_us");
+	let filler = pg_aboutus.querySelectorAll('[data-key]')
+	filler.forEach(element => {
+		let key = element.getAttribute('data-key');
+		element.innerHTML = page_aboutus.ro[key];
+	})
+}
+function switch_lang_en(){
+		if(document.getElementById("angol").checked == false){
+			document.getElementById("angol").checked = true;
+		}
+		document.getElementById("roman").checked = false;
+		document.getElementById("magyar").checked = false;
+
+	let pg_aboutus = document.getElementById("About_us");
+	let filler = pg_aboutus.querySelectorAll('[data-key]')
+	filler.forEach(element => {
+		let key = element.getAttribute('data-key');
+		element.innerHTML = page_aboutus.en[key];
+	})
+}
